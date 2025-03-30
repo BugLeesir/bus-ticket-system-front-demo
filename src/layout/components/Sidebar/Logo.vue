e34bc0e6430685f2c745f27f5284d2688d58c17f
Li YunRui <88717948+BugLeesir@users.noreply.github.com>
2025-03-31 04:54:49 +0800

    feat:增加网页logo标题

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,8 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Admin Template',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: '汽车购票系统',
+      logo: require('@/assets/logo/logo.png') // 引入 assets 下的 logo.png
     }
   }
 }
@@ -46,7 +46,7 @@ export default {
   width: 100%;
   height: 50px;
   line-height: 50px;
-  background: #2b2f3a;
+  background: #ffffff;
   text-align: center;
   overflow: hidden;
 
@@ -55,8 +55,8 @@ export default {
     width: 100%;
 
     & .sidebar-logo {
-      width: 32px;
-      height: 32px;
+      width: 54px;
+      height: 54px;
       vertical-align: middle;
       margin-right: 12px;
     }
@@ -64,7 +64,7 @@ export default {
     & .sidebar-title {
       display: inline-block;
       margin: 0;
-      color: #fff;
+      color: #304156;
       font-weight: 600;
       line-height: 50px;
       font-size: 14px;
